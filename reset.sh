a6ab0853a5ac2557cfe75157c98c8165c62c414b
TheWebAuthor <daniel@danielharris.net>
2020-04-22 18:56:06 -0400

    Reset whitelist in reset file

--- reset.sh
+++ reset.sh
@@ -1,4 +1,5 @@
 echo '' > /etc/dnsmasq.d/dnsmasq.adblocker.blacklist.yoyo.conf
 echo '' > /etc/dnsmasq.d/dnsmasq.adblocker.blacklist.notracking.conf
 echo '' > /etc/dnsmasq.d/dnsmasq.adblocker.blacklist.countries.conf
+echo '' > /etc/dnsmasq.d/dnsmasq.adblocker.whitelist.conf
 service dnsmasq restart
